820a7118700bcc149ec8f8f3461bf73790b37ab9
Dawnabelle <dawn.mott@gmail.com>
2018-04-27 15:25:45 -0700

    add result string to user output

--- src/backend.js
+++ src/backend.js
@@ -10,7 +10,7 @@ class Backend {
 
     // make an ajax request
     $.get({
-      url: 'https://api.betterdoctor.com/2016-03-01/doctors?last_name=' + doctorName + '&query=' + ailment + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + this.apiKey + 78,
+      url: 'https://api.betterdoctor.com/2016-03-01/doctors?last_name=' + doctorName + '&query=' + ailment + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + this.apiKey,
       success: function(data){
         callback(data);
       },
--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import './styles.css';
 $(function() {
 
   let backend = new Backend();
-  console.log(backend);
 
   // handler form input
   $('.doctor-form').on('submit', function(e){
@@ -31,7 +30,45 @@ $(function() {
       else if (!data.meta.total) {
         resultElm.html("I'm sorry... I'm so, so sorry. Your query returned no results.");
       }
-      console.log(data);
+
+      // when results contain doctors
+      else if (data.meta.total > 0) {
+        let resultString = "";
+
+      // iterate over results to form output resultString
+      $.each(data.data, function(idx, doctor){
+        let firstName = doctor.profile.first_name || "";
+        let lastName = doctor.profile.last_name || "";
+        let addressStreet = doctor.practices[0].visit_address.street || "";
+        let addressCity = doctor.practices[0].visit_address.city || "";
+        let addressState = doctor.practices[0].visit_address.state || "";
+        let addressZip = doctor.practices[0].visit_address.zip || "";
+        let address = addressStreet + ', ' + addressCity + ', ' + addressState + ', ' + addressZip || "";
+        let phoneNumber = doctor.practices[0].phones[0].number || "";
+        let website = doctor.practices[0].website || "";
+        let newPatients = doctor.practices[0].accepts_new_patients || "";
+
+        // test if doctor accepts new patients and replace with corresponding answer
+        if (newPatients) {
+          newPatients = "Yes";
+        } else {
+          newPatients = "No";
+        }
+        // build result string
+        resultString +=
+          '<p class="doctor-item">' +
+          firstName + ' ' + lastName + ' | ' +
+          phoneNumber + ' | ' +
+          address + ' | ' +
+          website + ' | ' +
+          'Accepting new patients? ' + newPatients +
+          '</p>';
+
+      });
+
+        // output the restult to html
+        resultElm.html(resultString);
+      }
 
     });
   });
